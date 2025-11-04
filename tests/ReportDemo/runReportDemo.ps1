d0ad8a5b0e876b411a8f8094631d92c5969f193b
mawosoft <78562192+mawosoft@users.noreply.github.com>
2025-11-04 04:25:47 +0100

    Add coverlet with SingleHit to report demo.

--- tests/ReportDemo/runReportDemo.ps1
+++ tests/ReportDemo/runReportDemo.ps1
@@ -4,14 +4,25 @@ param()
 [bool]$backupPreference = $PSNativeCommandUseErrorActionPreference
 try {
     $PSNativeCommandUseErrorActionPreference = $true
+    $reportParams = @(
+        "-reports:$PSScriptRoot/TestResults/logs/*/*cobertura.xml"
+        "-targetdir:$PSScriptRoot/TestResults/report"
+        '-reporttypes:Html,Markdown,Cobertura'
+    )
     dotnet build "$PSScriptRoot/ReportDemo.csproj"
     Remove-Item "$PSScriptRoot/TestResults" -Recurse -ErrorAction Ignore
-    dotnet test "$PSScriptRoot/ReportDemo.csproj" --no-build --settings "$PSScriptRoot/.runsettings" --collect 'XPlat Code Coverage'
-    reportgenerator "-reports:$PSScriptRoot/TestResults/logs/*/*cobertura.xml" "-targetdir:$PSScriptRoot/TestResults/report-coverlet" '-reporttypes:Html,Markdown,Cobertura'
+    dotnet test "$PSScriptRoot/ReportDemo.csproj" --no-build --collect 'XPlat Code Coverage'
+    reportgenerator $reportParams
     Rename-Item "$PSScriptRoot/TestResults/logs" 'logs-coverlet'
-    dotnet test "$PSScriptRoot/ReportDemo.csproj" --no-build --settings "$PSScriptRoot/.runsettings" --collect 'Code Coverage'
-    reportgenerator "-reports:$PSScriptRoot/TestResults/logs/*/*cobertura.xml" "-targetdir:$PSScriptRoot/TestResults/report-mscodecoverage" '-reporttypes:Html,Markdown,Cobertura'
+    Rename-Item "$PSScriptRoot/TestResults/report" 'report-coverlet'
+    dotnet test "$PSScriptRoot/ReportDemo.csproj" --no-build --collect 'XPlat Code Coverage' -- 'DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.SingleHit=true'
+    reportgenerator $reportParams
+    Rename-Item "$PSScriptRoot/TestResults/logs" 'logs-coverlet-singlehit'
+    Rename-Item "$PSScriptRoot/TestResults/report" 'report-coverlet-singlehit'
+    dotnet test "$PSScriptRoot/ReportDemo.csproj" --no-build --collect 'Code Coverage'
+    reportgenerator $reportParams
     Rename-Item "$PSScriptRoot/TestResults/logs" 'logs-mscodecoverage'
+    Rename-Item "$PSScriptRoot/TestResults/report" 'report-mscodecoverage'
 }
 finally {
     $PSNativeCommandUseErrorActionPreference = $backupPreference
