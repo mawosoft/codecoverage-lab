b6b5c3ef80d25e41ff5661aa95b7023b7b7125ec
mawosoft <78562192+mawosoft@users.noreply.github.com>
2025-11-06 03:09:41 +0100

    Implement TestCoverageDemoTests.

--- tests/TestCoverageDemoTests.cs
+++ tests/TestCoverageDemoTests.cs
@@ -1,14 +1,61 @@
 // Copyright (c) Matthias Wolf, Mawosoft.
 
+using System.Collections.Generic;
 using Xunit;
 
 namespace FooLib;
 
 public class TestCoverageDemoTests
 {
-    [Fact]
-    public void Test1()
+#pragma warning disable CA1034 // Nested types should not be visible
+#pragma warning disable CA1515 // Consider making public types internal
+    public class DemoTheoryData : TheoryData<int, int>
+#pragma warning restore CA1515 // Consider making public types internal
+#pragma warning restore CA1034 // Nested types should not be visible
     {
-        // TODO demo for TheoryData, ClassData, MemberData
+        public DemoTheoryData()
+        {
+            Add(1, 1);
+            Add(2, 2);
+        }
+    }
+
+    public static readonly object[][] DemoMemberDataField =
+    [
+        [1, 1],
+        [2, 2]
+    ];
+
+    public static IEnumerable<object[]> DemoMemberDataFunction(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            yield return [i, i];
+        }
+    }
+
+    [Theory]
+    [ClassData(typeof(DemoTheoryData))]
+    public void Test1(int p1, int p2)
+    {
+        Assert.Equal(p1, p2);
+    }
+
+    [Theory]
+#pragma warning disable xUnit1042 // The member referenced by the MemberData attribute returns untyped data rows
+    [MemberData(nameof(DemoMemberDataField))]
+#pragma warning restore xUnit1042 // The member referenced by the MemberData attribute returns untyped data rows
+    public void Test2(int p1, int p2)
+    {
+        Assert.Equal(p1, p2);
+    }
+
+    [Theory]
+#pragma warning disable xUnit1042 // The member referenced by the MemberData attribute returns untyped data rows
+    [MemberData(nameof(DemoMemberDataFunction), 2)]
+#pragma warning restore xUnit1042 // The member referenced by the MemberData attribute returns untyped data rows
+    public void Test3(int p1, int p2)
+    {
+        Assert.Equal(p1, p2);
     }
 }
