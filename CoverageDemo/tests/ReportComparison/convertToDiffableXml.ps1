592dd290b55146bdeb2ee73ea12ee5b4338b6b6e
mawosoft <78562192+mawosoft@users.noreply.github.com>
2025-11-17 19:39:19 +0100

    Refactor report comparison and create better diffables.

--- CoverageDemo/tests/ReportComparison/convertToDiffableXml.ps1
+++ CoverageDemo/tests/ReportComparison/convertToDiffableXml.ps1
@@ -1,12 +1,14 @@
 # Copyright (c) Matthias Wolf, Mawosoft.
 <#
 .SYNOPSIS
-    Converts an XML file to a diffable version.
-
-.NOTES
-    - Sorts attributes of all XML elements alphabetically.
-    - By default, sorts 'package' or 'module' elements alphabetically by 'name' attribute.
+    Converts a Cobertura or DynamicCoverage XML file to a diffable version.
 #>
+
+using namespace System.Collections
+using namespace System.Collections.Generic
+using namespace System.Linq
+using namespace System.Xml
+using namespace System.Xml.XPath
 
 [CmdletBinding(PositionalBinding = $false)]
 param(
@@ -16,42 +18,70 @@ param(
 
     [Parameter(Mandatory, Position = 1)]
     [ValidateNotNullOrWhiteSpace()]
-    [string]$Destination,
-
-    # Array of XPath queries for sorting subelements.
-    # Default values handle Cobertura and MS CodeCoverage XML format.
-    [hashtable[]]$ElementSort = @(
-        @{ xpath = '/coverage/packages/package/@name' }
-        @{ xpath = '/coverage/packages/package/classes/class/@name' }
-        @{ xpath = '/results/modules/module/@name' }
-        @{ xpath = '/results/skipped_modules/skipped_module/@name' }
-        @{ xpath = '/results/modules/module/functions/function/ranges/range/@start_column'; number = $true }
-        @{ xpath = '/results/modules/module/functions/function/ranges/range/@start_line'; number = $true }
-    )
+    [string]$Destination
 )
 
-$xmlDoc = [xml](Get-Content -LiteralPath $Path -Raw)
-foreach ($item in $ElementSort) {
-    $xpath = $item['xpath']
-    $nodes = $null
-    if ($item['number']) {
-        $nodes = [array]$xmlDoc.DocumentElement.SelectNodes($xpath) | Sort-Object { [int]$_.Value } -Stable
+function SortChildNodes {
+    param([XmlNode]$queryRoot, [string]$xPathQuery, [string[]]$sortQueries)
+
+    $xpath = [XPathExpression]::Compile($xPathQuery)
+    foreach ($sortQuery in $sortQueries) {
+        $xpath.AddSort($sortQuery, [Comparer]::DefaultInvariant)
     }
-    else {
-        $nodes = [array]$xmlDoc.DocumentElement.SelectNodes($xpath) | Sort-Object Value -Stable
+    $result = $queryRoot.CreateNavigator().Select($xpath)
+    if ($result.Count -eq 0) {
+        return
     }
-    if ($nodes) {
-        $nodes = $nodes.OwnerElement
-        # Group-Object doesn't group by reference equality
-        $groups = [System.Linq.Enumerable]::GroupBy[object, object]($nodes, { param($n)return $n.ParentNode }, [System.Collections.Generic.ReferenceEqualityComparer]::Instance).ToArray()
-        foreach ($group in $groups) {
-            $parent = $group.Key
-            $group.ForEach({ $null = $parent.RemoveChild($_) })
-            $group.ForEach({ $null = $parent.AppendChild($_) })
-        }
+    $result = [XmlNode[]]$result.GetNode()
+    # Group-Object doesn't group by reference equality
+    $groups = [Enumerable]::GroupBy[XmlNode, XmlNode]($result, { param($n) return $n.ParentNode }, [ReferenceEqualityComparer]::Instance).ToArray()
+    foreach ($group in $groups) {
+        $parent = $group.Key
+        $group.ForEach({ $null = $parent.RemoveChild($_) })
+        $group.ForEach({ $null = $parent.AppendChild($_) })
     }
 }
-$elems = [array]$xmlDoc.DocumentElement.SelectNodes('//*[@*]')
+
+$xmlDoc = [xml](Get-Content -LiteralPath $Path -Raw)
+$root = $xmlDoc.DocumentElement
+if ($root.get_Name() -eq 'coverage') {
+    SortChildNodes $root '/coverage/packages/package' -sortQueries '@name'
+    SortChildNodes $root '/coverage/packages/package/classes/class' -sortQueries "translate(@name, '<', ' ')", '@filename'
+    SortChildNodes $root '//lines/line' -sortQueries 'number(@number)'
+    SortChildNodes $root '/coverage/packages/package/classes/class/methods/method' -sortQueries 'number(lines/line[1]/@number)', '@name', '@signature'
+    $root.SelectNodes('//conditions/condition/@number').ForEach({ $_.Value = '0' })
+}
+elseif ($root.get_Name() -eq 'results') {
+    SortChildNodes $root '/results/*' -sortQueries 'name(.)'
+    SortChildNodes $root '/results/modules/module/*' -sortQueries 'name(.)'
+    SortChildNodes $root '/results/modules/module' -sortQueries '@name'
+    SortChildNodes $root '/results/skipped_modules/skipped_module' -sortQueries '@name'
+    SortChildNodes $root '/results/modules/module/skipped_functions/skipped_function' -sortQueries '@type_name', '@name'
+    SortChildNodes $root '/results/modules/module/source_files/source_file' -sortQueries '@path'
+    $modules = [XmlNode[]]$root.SelectNodes('/results/modules/module')
+    foreach ($module in $modules) {
+        $sourceIdMap = [Dictionary[string, string]]::new()
+        $sourceIds = [XmlNode[]]$module.SelectNodes('./source_files/source_file/@id')
+        for ([int]$i = 0; $i -lt $sourceIds.Length; $i++) {
+            $sourceId = $sourceIds[$i]
+            $sourceIdMap[$sourceId.Value] = $i
+            $sourceId.Value = $i
+        }
+        $sourceIds = [XmlNode[]]$module.SelectNodes('./functions/function/ranges/range/@source_id')
+        foreach ($sourceId in $sourceIds) {
+            $sourceId.Value = $sourceIdMap[$sourceId.Value]
+        }
+    }
+    $badNumbers = [XmlNode[]]$root.SelectNodes("(//@block_coverage|//@line_coverage)[contains(., ',')]")
+    foreach ($badNumber in $badNumbers) {
+        $badNumber.Value = $badNumber.Value.Replace([char]',', [char]'.')
+    }
+    SortChildNodes $root '//ranges/range' -sortQueries 'number(@source_id)', 'number(@start_line)', 'number(@start_column)'
+    SortChildNodes $root '/coverage/packages/package/classes/class/methods/method' -sortQueries 'number(lines/line[1]/@number)', '@name', '@signature'
+    SortChildNodes $root '/results/modules/module/functions/function' -sortQueries 'number(ranges/range[1]/@source_id)', 'number(ranges/range[1]/@start_line)', '@namespace', '@type_name', '@name'
+}
+
+$elems = [array]$root.SelectNodes('//*[@*]')
 foreach ($elem in $elems) {
     $attribs = $elem.Attributes | Sort-Object Name -Stable
     $elem.RemoveAllAttributes()
--- CoverageDemo/tests/ReportComparison/invokeReportComparison.ps1
+++ CoverageDemo/tests/ReportComparison/invokeReportComparison.ps1
@@ -9,6 +9,8 @@ try {
 
     $resultPath = "$PSScriptRoot/TestResults"
     $logPath = "$resultPath/logs"
+    $diagPath = "$resultPath/diag"
+    $diffablePath = "$resultPath/diffable"
     $reportPath = "$resultPath/report"
     $projectPath = "$PSScriptRoot/ReportComparison.csproj"
 
@@ -23,53 +25,105 @@ try {
         $projectParam = @($projectPath, '--configuration', $config)
         dotnet build $projectParam
 
-        $testId = 'coverlet'
-        dotnet test $projectParam $testParam 'XPlat Code Coverage'
-        $reportSource = "$resultPath/$testId.cobertura.xml"
+        $testId = 'coverlet-cobertura'
+        dotnet test $projectParam $testParam 'XPlat Code Coverage;Format=cobertura' --diag "$diagPath/$testId.log"
+        $reportSource = "$resultPath/$testId.xml"
         Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination $reportSource
-        Get-ChildItem -LiteralPath $logPath -Filter 'coverage.json' -Recurse | Move-Item -Destination "$resultPath/$testId.json"
         Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
         reportgenerator $reportParam "-reports:$reportSource"
         Rename-Item $reportPath "report-$testId"
 
-        $testId = 'coverlet-singlehit-rawmode'
-        dotnet test $projectParam $testParam 'XPlat Code Coverage' '--' 'DataCollectionRunSettings.DataCollectors.DataCollector[1].Configuration.SingleHit=true'
-        $reportSource = "$resultPath/$testId.cobertura.xml"
+        $testId = 'coverlet-cobertura-singlehit'
+        dotnet test $projectParam $testParam 'XPlat Code Coverage;Format=cobertura' --diag "$diagPath/$testId.log" '--' 'DataCollectionRunSettings.DataCollectors.DataCollector[1].Configuration.SingleHit=true'
+        $reportSource = "$resultPath/$testId.xml"
         Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination $reportSource
-        Get-ChildItem -LiteralPath $logPath -Filter 'coverage.json' -Recurse | Move-Item -Destination "$resultPath/$testId.json"
         Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
         reportgenerator $reportParam "-reports:$reportSource" 'settings:rawMode=true'
-        Rename-Item $reportPath "report-$testId"
+        Rename-Item $reportPath "report-$testId-rawmode"
 
-        $testId = 'mscc-multi'
-        dotnet test $projectParam $testParam 'Code Coverage;Format=cobertura,xml'
-        Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination "$resultPath/$testId.cobertura.xml"
-        Get-ChildItem -LiteralPath $logPath -Filter '*.xml' -Recurse | Move-Item -Destination "$resultPath/$testId.xml"
+        $testId = 'coverlet-json'
+        dotnet test $projectParam $testParam 'XPlat Code Coverage;Format=json' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter 'coverage.json' -Recurse | Move-Item -Destination "$resultPath/$testId.json"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+
+        $testId = 'coverlet-multi(json+cobertura)'
+        dotnet test $projectParam $testParam 'XPlat Code Coverage;Format=json,cobertura' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-cobertura.xml"
+        Get-ChildItem -LiteralPath $logPath -Filter 'coverage.json' -Recurse | Move-Item -Destination "$resultPath/$testId-json.json"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+
+        $testId = 'coverlet-multi(cobertura+json)'
+        dotnet test $projectParam $testParam 'XPlat Code Coverage;Format=cobertura,json' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-cobertura.xml"
+        Get-ChildItem -LiteralPath $logPath -Filter 'coverage.json' -Recurse | Move-Item -Destination "$resultPath/$testId-json.json"
         Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
 
         $testId = 'mscc-xml'
-        dotnet test $projectParam $testParam 'Code Coverage;Format=xml'
+        dotnet test $projectParam $testParam 'Code Coverage;Format=xml' --diag "$diagPath/$testId.log"
         $reportSource = "$resultPath/$testId.xml"
         Get-ChildItem -LiteralPath $logPath -Filter '*.xml' -Recurse | Move-Item -Destination $reportSource
         Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
         reportgenerator $reportParam "-reports:$reportSource"
         Rename-Item $reportPath "report-$testId"
+        dotnet-coverage merge $reportSource --output-format cobertura --output "$resultPath/merged-$testId-cobertura.xml"
 
         $testId = 'mscc-cobertura'
-        dotnet test $projectParam $testParam 'Code Coverage'
-        $reportSource = "$resultPath/$testId.cobertura.xml"
+        dotnet test $projectParam $testParam 'Code Coverage;Format=cobertura' --diag "$diagPath/$testId.log"
+        $reportSource = "$resultPath/$testId.xml"
         Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination $reportSource
         Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
         reportgenerator $reportParam "-reports:$reportSource"
         Rename-Item $reportPath "report-$testId"
-        $testId = 'mscc-cobertura-rawmode'
         reportgenerator $reportParam "-reports:$reportSource" 'settings:rawMode=true'
-        Rename-Item $reportPath "report-$testId"
+        Rename-Item $reportPath "report-$testId-rawmode"
+        dotnet-coverage merge $reportSource --output-format xml --output "$resultPath/merged-$testId-xml.xml"
 
-        $xmlfiles = Get-ChildItem -LiteralPath $resultPath -Filter '*.xml' | Select-Object -ExpandProperty FullName
-        foreach ($xmlfile in $xmlfiles) {
-            $destination = [System.IO.Path]::ChangeExtension($xmlfile, '.diffable.xml')
-            & "$PSScriptRoot/convertToDiffableXml.ps1" $xmlfile $destination
+        $testId = 'mscc-coverage'
+        dotnet test $projectParam $testParam 'Code Coverage;Format=coverage' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.coverage' -Recurse | Move-Item -Destination "$resultPath/$testId.coverage"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+        dotnet-coverage merge "$resultPath/$testId.coverage" --output-format cobertura --output "$resultPath/merged-$testId-cobertura.xml"
+        dotnet-coverage merge "$resultPath/$testId.coverage" --output-format xml --output "$resultPath/merged-$testId-xml.xml"
+
+        $testId = 'mscc-multi(coverage+cobertura)'
+        dotnet test $projectParam $testParam 'Code Coverage;Format=coverage,cobertura' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-cobertura.xml"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.coverage' -Recurse | Move-Item -Destination "$resultPath/$testId-coverage.coverage"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+
+        $testId = 'mscc-multi(cobertura+coverage)'
+        dotnet test $projectParam $testParam 'Code Coverage;Format=cobertura,coverage' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-cobertura.xml"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.coverage' -Recurse | Move-Item -Destination "$resultPath/$testId-coverage.coverage"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+
+        $testId = 'mscc-multi(xml+cobertura)'
+        dotnet test $projectParam $testParam 'Code Coverage;Format=xml,cobertura' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-cobertura.xml"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-xml.xml"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+
+        $testId = 'mscc-multi(cobertura+xml)'
+        dotnet test $projectParam $testParam 'Code Coverage;Format=cobertura,xml' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.cobertura.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-cobertura.xml"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-xml.xml"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+
+        $testId = 'mscc-multi(coverage+xml)'
+        dotnet test $projectParam $testParam 'Code Coverage;Format=coverage,xml' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.coverage' -Recurse | Move-Item -Destination "$resultPath/$testId-coverage.coverage"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-xml.xml"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+
+        $testId = 'mscc-multi(xml+coverage)'
+        dotnet test $projectParam $testParam 'Code Coverage;Format=xml,coverage' --diag "$diagPath/$testId.log"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.coverage' -Recurse | Move-Item -Destination "$resultPath/$testId-coverage.coverage"
+        Get-ChildItem -LiteralPath $logPath -Filter '*.xml' -Recurse | Move-Item -Destination "$resultPath/$testId-xml.xml"
+        Get-ChildItem -LiteralPath $logPath -Directory | Remove-Item -Recurse
+
+        $null = New-Item $diffablePath -ItemType Directory
+        Get-ChildItem -LiteralPath $resultPath -Filter '*.xml' | Select-Object -ExpandProperty Name | ForEach-Object {
+            & "$PSScriptRoot/convertToDiffableXml.ps1" "$resultPath/$_" "$diffablePath/$_"
         }
 
         Rename-Item $resultPath "TestResults-$config"
